6d0464e0178d018c1610da1d34d6f23fe4eb6a17
Darius Daniel <dariusdaniel771@gmail.com>
2023-12-04 12:20:48 +0100

    Adding script for task 0

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.isConnected = true;
+    this.client = createClient()
+      .on('error', (error) => {
+        this.isConnected = false;
+        console.log(error);
+      });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async get(key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.set).bind(this.client)(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await promisify(this.client.del).bind(this.client)(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
