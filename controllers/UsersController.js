f3e32b7f5403851d6b2705f93832ef9f531e007b
Darius Daniel <dariusdaniel771@gmail.com>
2023-12-05 14:28:04 +0100

    Fixed bug in endpoint /connect

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,8 +1,9 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
-  static postNew(request, response) {
+  static async postNew(request, response) {
     const { email, password } = request.body;
     const users = dbClient.db.collection('users');
 
@@ -10,16 +11,28 @@ class UsersController {
       response.status(400).send({ error: 'Missing email' });
     } else if (!password) {
       response.status(400).send({ error: 'Missing password' });
-    } else if (users.findOne({ email })) {
+    } else if (await (await users.findOne({ email }))) {
       response.status(400).send({ error: 'Already exist' });
     } else {
-      const newUser = users.insertOne({ email, password: sha1(password) });
-      response.status(201).send({ id: newUser.InsertId, email });
+      const newUser = await (await users.insertOne({ email, password: sha1(password) }));
+      response.status(201).send({ id: newUser.insertedId, email });
     }
   }
 
   static getMe(request, response) {
-    const token = request.headers['X-Token'];
+    const token = request.header('X-Token');
+    const key = `auth_${token}`;
+    const users = dbClient.db.collection('users');
+    const user = redisClient.get(key);
+
+    if (!users.findOne(user)) {
+      response.status(401).send({ error: 'Unauthorized' });
+    } else {
+      response.status(201).send({
+        email: users.findOne(user).email,
+        id: users.findOne(user)._id,
+      });
+    }
   }
 }
 
